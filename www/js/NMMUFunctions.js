8d866b7d12c919866ea34cdf80509e6bd519ae91
KingShaun <shaun@nmmu.ac.za>
2017-02-16 14:36:32 +0200

    Geo loc 2

--- www/js/NMMUFunctions.js
+++ www/js/NMMUFunctions.js
@@ -192,9 +192,31 @@ function onDeviceReady() {
         }
     });
 
+    // onSuccess Geolocation
+    //
+    function onSuccess(position) {
+        var element = document.getElementById('LiWhereAmI');
+        element.innerHTML = 'Latitude: ' + position.coords.latitude + '<br />' +
+                            'Longitude: ' + position.coords.longitude + '<br />' +
+                            'Altitude: ' + position.coords.altitude + '<br />' +
+                            'Accuracy: ' + position.coords.accuracy + '<br />' +
+                            'Altitude Accuracy: ' + position.coords.altitudeAccuracy + '<br />' +
+                            'Heading: ' + position.coords.heading + '<br />' +
+                            'Speed: ' + position.coords.speed + '<br />' +
+                            'Timestamp: ' + position.timestamp + '<br />';
+    }
+
+    // onError Callback receives a PositionError object
+    //
+    function onError(error) {
+        alert('code: ' + error.code + '\n' +
+              'message: ' + error.message + '\n');
+    }
+
     $(document).on("pagebeforeshow", "#PageLoggedInHome", function () {
         //Get user's location
-        navigator.geolocation.getCurrentPosition(locwhereamiSuccess, locwhereamiError);
+        //navigator.geolocation.getCurrentPosition(locwhereamiSuccess, locwhereamiError);
+        navigator.geolocation.getCurrentPosition(onSuccess, onError);
     });
 
 

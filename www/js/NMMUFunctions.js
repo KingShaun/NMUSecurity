b483391ef467ae0d4f81520e6874fbd862f873ea
KingShaun <shaun@nmmu.ac.za>
2017-02-20 11:11:00 +0200

    Removed auto submit

--- www/js/NMMUFunctions.js
+++ www/js/NMMUFunctions.js
@@ -660,7 +660,7 @@ function GetADDetailsForEmergencyEmail(username, password) {
     }).done(function (msg) {
         $("#NameEmergencyEmail", formEmergencyEmail).val(msg.d.FullName);
         $("#EmailEmergencyEmail", formEmergencyEmail).val(msg.d.Email);
-        $('#FormEmergencyEmail').submit();
+        //$('#FormEmergencyEmail').submit();
 
     }).fail(function (msg) {
         navigator.notification.alert("An error has occurred.", function () { });
